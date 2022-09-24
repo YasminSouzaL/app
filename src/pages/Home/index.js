eba77a1dbe0f9a068b329a0ea594c8cc579f3914
Yasmin Souza <souzasmimi@gmail.com>
2022-09-24 16:12:19 -0300

    ok

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,12 +1,47 @@
 import React from 'react';
-import {Container, Title} from '../../styles';
+import { TouchableOpacity } from 'react-native';
+import {Container, Avatar, Map} from '../../styles';
 
 const Home = () => {
     return (
         <Container>
-            <Title>Home</Title>
+            <Map 
+                initialRegion={{
+                    latitude: -30.011364,
+                    longitude: -51.16373
+                }}
+            />
+            <Container
+                position="absolute"
+                justify="space-between"
+                align="flex-start"
+                padding={20}
+                zIndex={999}
+                pointerEvents="box-none"
+                style={{height: '100%'}}
+            >
+                {/* Parte superior*/}
+                <Container 
+                    height={100} 
+                    justify="flex-start"
+                    align="flex-start"
+                    color="info"
+                >
+                    {/*AVATAR*/}
+                    <TouchableOpacity>
+                        <Avatar source={{ uri: ''}} />
+                    </TouchableOpacity>
+                </Container>
+
+            </Container>
         </Container>
     );
 }
-
+/* 
+query={{
+  //GOOGLE_MAPS_APIKEY
+  key: GOOGLE_MAPS_APIKEY,
+  language: 'pt-BR'
+}}
+*/
 export default Home;
--- src/styles/index.js
+++ src/styles/index.js
@@ -1,5 +1,7 @@
 import styled from "styled-components/native";
 import theme from "./theme.json";
+import GOOGLE_MAPS_APIKEY from '.env';
+import MapView from 'react-native-maps';
 
 export const Container = styled.View`
   flex: 1;
@@ -79,5 +81,23 @@ export const AddressList = styled.FlatList`
 export const AddressItem = styled.TouchableOpacity`
   padding: 5px 0;
   align-items: flex-start;
+`;
 
+export const Map = styled(MapView)`
+  flex: 1;
+  width: 100%;
+  height: 100%;
+  opacity: ${(props) => (props.disabled ? 0.2 : 1)};
+  query: ${{
+    key: AIzaSyCG_bL-UfXM-Hdq2saUNoRht5dezqn1xbA,
+    language: 'pt-BR'
+  }}
+`;
+
+export const Avatar = styled.Image`
+  width: ${(props) => (props.small ? '35px' : '50px')};
+  height: ${(props) => (props.small ? '35px' : '50px')};
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  background: ${theme.colors.muted};
+  border-radius: ${(props) => (props.small ? '35px' : '50px')};
 `;
