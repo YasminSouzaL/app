40634e9029555e224ea675974dba147b6fd98acb
Yasmin Souza <souzasmimi@gmail.com>
2022-10-08 12:47:03 -0300

    ok

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,74 +1,52 @@
-import React from 'react';
-import { TouchableOpacity } from 'react-native';
-import {Container, Avatar, Map, Title, SubTitle, Spacer, Input} from '../../styles';
-//import GOOGLE_MAPS_APIKEY from '.env';
+import React, {useState,useEffect} from 'react';
+import { Keyboard} from 'react-native';
 
-const Home = () => {
+import { Container, Button, 
+    ButtonText, Title, SubTitle, 
+    Input, Spacer} from '../styles';
 
-    const tipo = 'P';
-    const tipo2 = 'M';
+const Passenger = () => {    
+    const [visible, setvisible] = useState(true); 
 
-    return (
-        <Container>
-            <Map 
-                initialRegion={{
-                    latitude: -30.011364,
-                    longitude: -51.16373
-                }}
-            />
-            <Container
-                position="absolute"
-                justify="space-between"
-                align="flex-start"
-                padding={20}
-                zIndex={999}
-                pointerEvents="box-none"
-                style={{height: '100%'}}
-            >
-                {/* Parte superior*/}
-                <Container 
-                    height={100} 
-                    justify="flex-start"
-                    align="flex-start">
-                    {/*AVATAR*/}
-                    <TouchableOpacity>
-                        <Avatar source={{ uri: 'https://i.pinimg.com/originals/65/71/07/6571072297e92351469e0cc39ac1500b.png'}} />
-                    </TouchableOpacity>
-                </Container>
-                <Container 
-                    justify="flex-start" 
-                    padding={30}
-                    align="flex-start" 
-                    elevation={50} 
-                    height={150}
-                    color="light"
-                >
-                    {/* PASSAGEIRO SEM CORRIDA */}
-                    {tipo == "P" && (
-                        <Container>
-                            <SubTitle>Olá,MIMI</SubTitle>
-                            <Title>Pra onde você quer ir?</Title>
-                            <Spacer />
-                            <Input placeholder="Procure um destino..."/> 
-                        </Container>
-                    )}
-                    {/* MOTORISTA SEM CORRIDA */}   
-                    {tipo == "M" &&( 
-                        <Container>
-                            <SubTitle>Olá,MIMI</SubTitle>
-                            <Title>Nenhum corrida encontrada.</Title>    
-                        </Container>
-                    )}
-                </Container>
+    useEffect(() => {
+        const keyboardDidShowListener = Keyboard.addListener(
+            'keyboardDidShow', 
+            () => setvisible(false),
+        );
+        const keyboardDidHideListener = Keyboard.addListener(
+            'keyboardDidHide', 
+            () => setvisible(true),
+        );
+        return () => {
+            keyboardDidShowListener.remove();
+            keyboardDidHideListener.remove();
+        };
+    },[]);
+
+    return(
+        <Container padding={40} justify="flex-start">
+            <Container align="flex-start" height={80} >
+                <Title>Cadastre</Title>
+                <SubTitle>Preencha os campos abaixo.</SubTitle>
             </Container>
+            <Container justify="flex-start">
+                <Spacer height={40} />
+                <Input placeholder="Nome" />
+                <Spacer height={20} />
+                <Input placeholder="Curso" />
+                <Spacer height={20} />
+                <Input placeholder="email" />
+                <Spacer height={20} />
+            </Container>
+            {visible && (
+                <Container height={70} justify="flex-end">
+                    <Button>
+                        <ButtonText>Começar a usar</ButtonText>
+                    </Button>
+                </Container>
+            )}
         </Container>
     );
 }
-/* 
-query={{
-  //GOOGLE_MAPS_APIKEY
-  key: GOOGLE_MAPS_APIKEY,
-  language: 'pt-BR'
-}}
-*/
-export default Home;
+
+export default Passenger;
