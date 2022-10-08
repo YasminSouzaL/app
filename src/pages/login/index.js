ea4e080c9a1eb1c75d77ffaee32d2525e0a36639
Yasmin Souza <souzasmimi@gmail.com>
2022-10-08 13:40:37 -0300

    ok

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,13 +1,13 @@
 import React,{useState} from 'react';
-import {Text,Image,StyleSheet,Checkbox} from 'react-native';
-import {Container,Input} from '../../styles';
+import {Text,Image,StyleSheet,Checkbox, View,Input} from 'react-native';
+import {Container} from '../../styles';
 
 import logo from '../../../assets/logo.png';
 import fundo from '../../../assets/fundo_tela.jpg'
 
 const Login = () => {
     const [isSelected, setSelection] = useState(false);
-
+    
     return (
         <Container color="info50" justify="flex-end">
             <Container
@@ -17,14 +17,17 @@ const Login = () => {
                height={400}
                top={0}
                zIndex={9}>
-                <Image source={logo} />
-                    <Input color="light">Email</Input>
-                    <Input color="info">Senha</Input>
-                    <Checkbox
-                        value={isSelected}
-                        onValueChange={setSelection}
-                        style={styles.checkboxContainer}
-                    />   
+                    <Image source={logo} />
+                    <Input>Email</Input>
+                    <Input>Senha</Input>
+                    <View style={styles.checkbox}>
+                        <Checkbox
+                            value={isSelected}
+                            onValueChange={setSelection}
+                            style={styles.checkboxContainer}
+                        />
+                        <Text style={styles.label}>Você é motorista</Text>   
+                    </View>      
             </Container>
             <Image source={fundo}/>
         </Container>
@@ -32,11 +35,16 @@ const Login = () => {
 };
 
 const styles = StyleSheet.create({
+    checkbox: {
+        alignSelf: "center",
+    },
     checkboxContainer: {
         flexDirection: "row",
         marginBottom: 20,
-    }
-
+    },
+    label: {
+        margin: 8,
+    },
 });
 
 export default Login;
