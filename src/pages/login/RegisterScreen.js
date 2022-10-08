2318f861ad1513d27849522ffa50d6f54f82d473
Yasmin Souza <souzasmimi@gmail.com>
2022-10-08 13:12:54 -0300

    ok

--- src/pages/login/RegisterScreen.js
+++ src/pages/login/RegisterScreen.js
@@ -1,10 +1,12 @@
 import React from "react";
-import { View } from "react-native";
+import { View,Text } from "react-native";
 
 const RegisterScreen = () =>{
     return (
         <View>
-            
+            <Text>RegisterScreen</Text>
         </View>
     )
 }
+
+export default RegisterScreen;
