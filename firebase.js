e13c1e4af23dbcdb4a16ecda517155d6e952fb39
Yasmin Souza <souzasmimi@gmail.com>
2022-10-11 22:27:44 -0300

    ok

--- firebase.js
+++ firebase.js
@@ -18,6 +18,7 @@ const firebaseConfig = {
     messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
     appId: FIREBASE_APP_ID
 }
+console.log(firebaseConfig);
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
