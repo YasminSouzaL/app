270da3cb635ae29963879af67009cd3acaa6daf2
Yasmin Souza <70536067+mimi124515@users.noreply.github.com>
2022-11-29 13:41:06 -0300

    Update type.js

--- src/pages/Steps/type.js
+++ src/pages/Steps/type.js
@@ -1,34 +1,107 @@
-import React from 'react';
-import { Image, Text } from 'react-native';
+import React, {useState,useEffect} from 'react';
+import { Keyboard} from 'react-native';
+import { useNavigation } from "@react-navigation/native";
+import { Container, Button, 
+    ButtonText, Title, SubTitle, 
+    Input, Spacer} from '../../styles';
 
-import { Container, Button, ButtonText, Title, SubTitle, PickerButton} from '../../styles';
+import { auth,db } from '../../../firebase';
+import { collection, addDoc } from 'firebase/firestore';
+//import { setDoc, doc } from 'firebase/firestore';
 
-import car from '../../../assets/car.jpg';
-import hand from '../../../assets/harder.jpg';
-const Type = () => {
+const driver = () => {    
+    const [visible, setvisible] = useState(true); 
+    const [name, setName] = useState(''); 
+    const [curso, setCurso] = useState('');
+
+    const navigator = useNavigation();   
+
+    const [driver, setDriver] = useState({
+        passeger: auth.currentUser.uid,
+        name: '',
+        rg: '',
+        telefone: '',
+    })
+
+    const newDriver = () => {
+        addDoc(collection(db, "driver"), {
+            driver: auth.currentUser.uid,
+            name: driver.name,
+            rg: driver.rg,
+            telefone: diver.telefone,
+        })
+        .then(() => {
+            console.log("Document successfully written!");
+            navigator.navigate('Card');
+        })
+        .catch((error) => {
+            console.error("Error writing document: ", error);
+        });
+    }
+
+    useEffect(() => {
+        /*const unsubscribe = auth.onAuthStateChanged((user) => {
+            if (user) navigator.navigate('Home');
+        });*/
+        const keyboardDidShowListener = Keyboard.addListener(
+            'keyboardDidShow', 
+            () => setvisible(false),
+        );
+        const keyboardDidHideListener = Keyboard.addListener(
+            'keyboardDidHide', 
+            () => setvisible(true),
+        );
+        return () => {
+            keyboardDidShowListener.remove();
+            keyboardDidHideListener.remove();
+        };
+    },[]);
+
+
+    
     return(
-        <Container padding={30} justify="flex-start">
+        <Container padding={40} justify="flex-start">
             <Container align="flex-start" height={80} >
-                <Title>Passageiro ou motorista?</Title>
-                <SubTitle>Selecione qual será a sua função.</SubTitle>
+                <Title>Cadastre</Title>
+                <SubTitle>Preencha os campos abaixo.</SubTitle>
             </Container>
-            <Container>
-                <PickerButton active>
-                    <Image source={car} />
-                    <Title>Motorista</Title>
-                </PickerButton>
-                <PickerButton>
-                    <Image source={hand} />
-                    <Title>Passageiros</Title>
-                </PickerButton>
+            <Container justify="flex-start">
+                <Spacer height={20} />
+                <Input 
+                    placeholder="Nome" 
+                    value={driver.name}
+                    onChangeText={(itemValue) => setDriver({...driver, name: itemValue})}
+                />
+                <Spacer height={20} />
+                <Input 
+                    placeholder="RG" 
+                    value={driver.rg}
+                    onChangeText={(itemValue) => setDriver({...driver, rg: itemValue})}
+                    
+                />
+                <Spacer height={20} />
+                <Input 
+                    placeholder="Telefone"
+                    value={driver.telefone}
+                    onChangeText={(itemValue) => setDriver({...driver, telefone: itemValue})}
+                />
+                <Spacer height={20} />
             </Container>
-            <Container height={70} justify="flex-end">
-                <Button>
-                    <ButtonText>Próximo passo</ButtonText>
-                </Button>
-            </Container>
+                <Container 
+                    height={70} 
+                    justify="flex-end"
+                    align="flex-end"
+                >
+                    {visible && (
+                        <Button 
+                            onPress={() => newDriver()}
+                        >
+                            <ButtonText>Começar a usar</ButtonText>
+                        </Button>
+                    )}
+                </Container>
         </Container>
     );
 }
 
-export default Type;
+export default driver;
